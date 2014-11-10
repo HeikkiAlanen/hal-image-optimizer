9f9119d4a0da81cac2f601eadb64cbda5cab3f35
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-11-10 13:13:17 +0200

    Grunt-istanbul added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@
 
 /*global module*/
 module.exports = function(grunt) {
-
     grunt.initConfig({
 		eslint: {
 			options: {
@@ -13,15 +12,42 @@ module.exports = function(grunt) {
 			},
 			target: ['*.js', './lib/*.js']
 		},
-        clean: ['./test/test_images/thumbnails'],
+        clean: ['./test/test_images/thumbnails', './coverage'],
 		nodeunit: {
 			all: ['./test/*_spec.js']
-		}
-	});
+		},
+        env: {
+            coverage: {
+                APP_DIR_FOR_CODE_COVERAGE: '../coverage/instrument/lib/'
+            }
+        },
+        instrument: {
+            files: 'lib/*.js',
+            options: {
+                lazy: true,
+                basePath: 'coverage/instrument/'
+            }
+        },
+        storeCoverage: {
+            options: {
+                dir: 'coverage/reports'
+            }
+        },
+        makeReport: {
+            src: 'coverage/reports/**/*.json',
+            options: {
+                type: 'lcov',
+                dir: 'coverage/reports',
+                print: 'detail'
+            }
+        }
+    });
 
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-istanbul');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.registerTask('default', ['eslint']);
   grunt.registerTask('test', ['eslint', 'clean', 'nodeunit']);
+  grunt.registerTask('coverage', ['env:coverage', 'instrument', 'nodeunit', 'storeCoverage', 'makeReport']);
   };
