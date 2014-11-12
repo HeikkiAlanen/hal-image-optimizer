2ab0ded96baeef4c15ea01f9910fd022372d52d0
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-11-12 12:24:25 +0200

    Code coverage results pushed to Coveralls.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,43 +12,15 @@ module.exports = function(grunt) {
 			},
 			target: ['*.js', './lib/*.js']
 		},
-        clean: ['./test/test_images/thumbnails', './coverage'],
+        clean: ['./test/test_images/thumbnails'],
 		nodeunit: {
 			all: ['./test/*_spec.js']
-		},
-        env: {
-            coverage: {
-                APP_DIR_FOR_CODE_COVERAGE: '../coverage/instrument/lib/'
-            }
-        },
-        instrument: {
-            files: 'lib/*.js',
-            options: {
-                lazy: true,
-                basePath: 'coverage/instrument/'
-            }
-        },
-        storeCoverage: {
-            options: {
-                dir: 'coverage/reports'
-            }
-        },
-        makeReport: {
-            src: 'coverage/reports/**/*.json',
-            options: {
-                type: 'lcov',
-                dir: 'coverage/reports',
-                print: 'detail'
-            }
-        }
+		}
     });
 
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-istanbul');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-env');
   grunt.registerTask('default', ['eslint']);
   grunt.registerTask('test', ['eslint', 'clean', 'nodeunit']);
-  grunt.registerTask('coverage', ['clean', 'env:coverage', 'instrument', 'nodeunit', 'storeCoverage', 'makeReport']);
   };
