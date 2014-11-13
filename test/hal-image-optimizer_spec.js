0a95feb78851ceb1527550c8303e6bf6a91bb69c
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-11-13 12:00:51 +0200

    Added unit test cases.

--- test/hal-image-optimizer_spec.js
+++ test/hal-image-optimizer_spec.js
@@ -1,21 +1,33 @@
 var imgOptimizer = require('../lib/hal-image-optimizer.js');
-var fs = require("fs");
+var fs = require("fs-extra");
 
+var testFolder = "./test/test_images";
 
 exports.imgOptimizer = function(test) {
-		test.expect(2);
+		test.expect(3);
 		
+        // Test 1
 		var value1 = imgOptimizer('./test/images');
 		test.strictEqual(value1, false, "Image folder not found");
 		
+        // Test 2
 		var value2 = imgOptimizer('images');
 		test.strictEqual(value2, false, "Image folder not found");
+        
+        // Test 3
+        // Delete possible thumbnails folder before testing
+        fs.removeSync(testFolder + "/thumbnails");
+        var value3 = imgOptimizer(testFolder);
+        // Check if thumbnails folder created
+        var folder1 = fs.existsSync(testFolder + "/thumbnails");
+        test.equal(folder1, true, "Thumbnails folder created OK.");
 
+        // Test 4
 		// Compare source file and thumbnail count
-        // 23.10.14/HAl: For some reason .execFile command is not working with unit testing (in easyimage.js)
-		//var value2 = imgOptimizer('./test/test_images');
-		//var imgFiles = fs.readdirSync('./test/test_images');
-		//var thumbFiles = fs.readdirSync('./test/test_images/thumbnails');
+        //fs.removeSync(testFolder + "/thumbnails");
+		//var value4 = imgOptimizer(testFolder);
+		//var imgFiles = fs.readdirSync(testFolder);
+		//var thumbFiles = fs.readdirSync(testFolder + "/thumbnails");
 		//test.equal(imgFiles.length-1, thumbFiles.length, "Thumbnails created OK."); // Excluding thumbnails folder
 		
 		test.done();
