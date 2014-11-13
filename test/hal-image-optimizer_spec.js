f4c937245f3cdcc183682fc63a93fa05e3120414
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-11-13 16:18:52 +0200

    User defined target directory added.

--- test/hal-image-optimizer_spec.js
+++ test/hal-image-optimizer_spec.js
@@ -2,6 +2,7 @@ var imgOptimizer = require('../lib/hal-image-optimizer.js');
 var fs = require("fs-extra");
 
 var testFolder = "./test/test_images";
+var trgtFolder = "./test/test_images/thumbnails"
 
 exports.imgOptimizer = function(test) {
 		test.expect(4);
@@ -17,15 +18,16 @@ exports.imgOptimizer = function(test) {
         // Test 3, create thumbnails folder if not existing
         // Delete possible thumbnails folder before testing
         fs.removeSync(testFolder + "/thumbnails");
-        var value3 = imgOptimizer(testFolder);
+        fs.removeSync(trgtFolder);
+        var value3 = imgOptimizer(testFolder, trgtFolder);
         // Check if thumbnails folder created
-        var folder1 = fs.existsSync(testFolder + "/thumbnails");
+        var folder1 = fs.existsSync(trgtFolder);
         test.equal(folder1, true, "Thumbnails folder created OK.");
 
         // Test 4, thumbnails folder existing
-        var value4 = imgOptimizer(testFolder);
+        var value4 = imgOptimizer(testFolder, trgtFolder);
         // Check if thumbnails folder still exists
-        var folder2 = fs.existsSync(testFolder + "/thumbnails");
+        var folder2 = fs.existsSync(trgtFolder);
         test.equal(folder2, true, "Thumbnails folder OK.");
 
         // Test 5
