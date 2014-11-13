b5ec37765a41eee233dcb6a64a53d362ddd799fc
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-11-13 13:11:10 +0200

    One new unit test case added.

--- test/hal-image-optimizer_spec.js
+++ test/hal-image-optimizer_spec.js
@@ -4,17 +4,17 @@ var fs = require("fs-extra");
 var testFolder = "./test/test_images";
 
 exports.imgOptimizer = function(test) {
-		test.expect(3);
+		test.expect(4);
 		
-        // Test 1
+        // Test 1, non existent folder ./ format
 		var value1 = imgOptimizer('./test/images');
 		test.strictEqual(value1, false, "Image folder not found");
 		
-        // Test 2
+        // Test 2, non existent folder without ./
 		var value2 = imgOptimizer('images');
 		test.strictEqual(value2, false, "Image folder not found");
         
-        // Test 3
+        // Test 3, create thumbnails folder if not existing
         // Delete possible thumbnails folder before testing
         fs.removeSync(testFolder + "/thumbnails");
         var value3 = imgOptimizer(testFolder);
@@ -22,7 +22,13 @@ exports.imgOptimizer = function(test) {
         var folder1 = fs.existsSync(testFolder + "/thumbnails");
         test.equal(folder1, true, "Thumbnails folder created OK.");
 
-        // Test 4
+        // Test 4, thumbnails folder existing
+        var value4 = imgOptimizer(testFolder);
+        // Check if thumbnails folder still exists
+        var folder2 = fs.existsSync(testFolder + "/thumbnails");
+        test.equal(folder2, true, "Thumbnails folder OK.");
+
+        // Test 5
 		// Compare source file and thumbnail count
         //fs.removeSync(testFolder + "/thumbnails");
 		//var value4 = imgOptimizer(testFolder);
