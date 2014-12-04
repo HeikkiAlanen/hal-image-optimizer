69a730bdad5c48aa7d3cc600a16da97399f9e6ab
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-12-04 13:15:43 +0200

    Added possibility to read options from file. Unit tests updated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,15 +12,24 @@ module.exports = function(grunt) {
 			},
 			target: ['*.js', './lib/*.js']
 		},
-        clean: ['./test/test_images/thumbnails'],
+        clean: ['./test/test_images/thumbnails', './hal-image-optimizer.json'],
 		nodeunit: {
 			all: ['./test/*_spec.js']
-		}
+		},
+        "file-creator": {
+            "config1": {
+                "./hal-image-optimizer.json": function(fs, fd, done) {
+                    fs.writeSync(fd, '{ "width": "200", "height": "200" }');
+                    done();
+                }
+            }
+        }
     });
 
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-file-creator');
   grunt.registerTask('default', ['eslint']);
-  grunt.registerTask('test', ['eslint', 'clean', 'nodeunit']);
+  grunt.registerTask('test', ['eslint', 'clean', 'nodeunit', 'clean', 'file-creator:config1', 'nodeunit']);
   };
--- test/hal-image-optimizer_spec.js
+++ test/hal-image-optimizer_spec.js
@@ -1,19 +1,19 @@
 var imgOptimizer = require('../lib/hal-image-optimizer.js');
 var fs = require("fs-extra");
 
-//var testFolder = "./test/test_images";
-//var trgtFolder;
-
 // Remove test directories just in case
-//trgtFolder = "./test/test_images/thumbnails3";
 fs.removeSync("./test/test_images/thumbnails3");
-//trgtFolder = "./test/test_images/thumbnails4";
 fs.removeSync("./test/test_images/thumbnails4");
-//trgtFolder = "./test/test_images/thumbnails5";
+fs.removeSync("./test/test_images/thumbnails5");
 fs.removeSync("./test/test_images/thumbnailsX");
 
+var confFile = 'hal-image-optimizer.json';
+if (fs.existsSync(confFile)) {
+    var koe = fs.renameSync(confFile, "OLD_" + confFile);
+}
+
 exports.imgOptimizer = function(test) {
-    test.expect(5);
+    test.expect(7);
 
     // Test 1, non existent folder ./ format
     var options1 = {
@@ -53,6 +53,31 @@ exports.imgOptimizer = function(test) {
     var folder2 = fs.existsSync(options4.target);
     test.equal(folder2, true, "Thumbnails folder not existing.");
 
+    // Test 5, configuration file exists, but it is empty
+    var options5 = {
+        "source": "./test/test_images",
+        "target": "./test/test_images/thumbnails5"
+    }
+    fs.writeFileSync(confFile, 
+        '{ }');
+    var value5 = imgOptimizer(options5);
+    // Check if thumbnails folder exists
+    var folder5 = fs.existsSync("./test/test_images/thumbnails5");
+    test.equal(folder5, true, "Thumbnails folder not existing.");
+
+    // Test 6, configuration file exists
+    var options6 = {
+    }
+    fs.writeFileSync(confFile, 
+        '{ "source": "./test/test_images", "target": "./test/test_images/thumbnails6", "width": "200", "height": "200"}');
+    var value6 = imgOptimizer(options6);
+    // Check if thumbnails folder exists
+    var folder6 = fs.existsSync("./test/test_images/thumbnails6");
+    test.equal(folder6, true, "Thumbnails folder not existing.");
+    if (fs.existsSync("OLD_" + confFile)) {
+        fs.renameSync("OLD_" + confFile, confFile);
+    }
+
     // Test x (MUST BE THE LAST TEST CASE)
     // Compare source file count and created thumbnails count
 //    trgtFolder = "./test/test_images/thumbnails5";
@@ -64,7 +89,7 @@ exports.imgOptimizer = function(test) {
         function(image) {
             var imgFiles = fs.readdirSync(optionsX.source);
             var thumbFiles = fs.readdirSync(optionsX.target);
-            test.equal(imgFiles.length-3, thumbFiles.length, "Thumbnails created OK."); // Excluding thumbnails folder
+            test.equal(imgFiles.length-5, thumbFiles.length, "Thumbnails created OK."); // Excluding thumbnails folder
             test.done();
         },
         function(err) {
